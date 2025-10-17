4430ce643fa1b672c59066f57ead786ff20bc136
Moshabata-Lithakong <lithakongjoseph@gmail.com>
2025-10-17 05:03:02 +0200

    Fix Vercel SPA routing

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "703475edec2d7e85a36a82ad5c18fe88
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "15b5c3e87384dbb557b8bc6c82eec472",
+"flutter_bootstrap.js": "df7ea50a36c5ed7fd18f237ead28c09c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
